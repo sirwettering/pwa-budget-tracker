12ac745b4ff85adaf95bd494df02edd04655d201
William Wettering <sirwettering@github.com>
2022-01-18 21:43:55 -0500

    updated routes and mongo

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,13 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://wwettering:<916919u916919W>@pwa-budget-tracker.yyfvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
